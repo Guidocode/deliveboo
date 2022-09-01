556668f2ec5c7f5619640f25f19099271c4c876a
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-09-01 16:13:38 +0200

    Creata card piatti creata card ristorasnti

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -9,8 +9,8 @@
     <FooterComp/>
 
   </div>
-  
-  
+
+
 </template>
 
 <script>
@@ -40,6 +40,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+main{
+    padding-top: 100px;
+}
 </style>
