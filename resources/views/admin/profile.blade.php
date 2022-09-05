b3bfa7b925dce0ce6a955dc70d6948b1a2492d56
ketjj <jorjolianiket@gmail.com>
2022-09-05 19:14:19 +0200

    add routes

---
+++ resources/views/admin/profile.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.admin')
+@section('content')
+  my-profile
+@endsection
+@section('title', 'my-profile')
---
+++ resources/views/admin/reports.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.admin')
+@section('content')
+  reports
+@endsection
+@section('title', 'reports')
