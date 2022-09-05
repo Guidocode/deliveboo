bcac238a1a06f17488d9574f648c89000d615cc5
ketjj <jorjolianiket@gmail.com>
2022-09-05 03:14:33 +0200

    add category styles

--- resources/js/components/pages/RestaurantComp.vue
+++ resources/js/components/pages/RestaurantComp.vue
@@ -1,6 +1,33 @@
 <template>
-    <div>
-        <div class="search-container p-1">
+  <div class="restaurants text-center">
+    
+        <!-- <h2>Cosa vuoi mangiare?</h2> -->
+
+        <div class="input-wrap">
+          <div class="inputs">
+            <div id="ck-button" v-for="(tipo, index) in types" :key="`tipo${ index }`">
+              <label :for="tipo.id">
+                <input type="checkbox" :name="tipo.name" :id="tipo.id" :value="tipo.id" v-model="selectedTypes"><span><i class="fa-solid fa-circle-check"></i> {{ tipo.name }}</span>
+              </label>
+            </div>
+          </div>
+        </div>
+
+      <!-- <div class="line mt-2"></div> -->
+      <div class="my_container d-flex flex-wrap justify-content-between">
+                <div v-for="resturant in resturants" :key="resturant.id" class="card" style="width: 18rem;">
+            <img :src="resturant.image" class="card-img-top" alt="">
+            <div class="card-body">
+                <h5 class="card-title">{{ resturant.name }}</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <router-link class="btn btn-primary" :to="{ name: 'Menu', params: {slug: resturant.slug} }" >Vai al ristorante</router-link>
+            </div>
+         </div>
+
+      </div> 
+
+
+        <!-- <div class="search-container p-1">
             <h4 class=" text-center">Cosa vuoi mangiare?</h4>
             <ul class="d-flex justify-content-center">
                 <li class=" ml-2" v-for="(tipo, index) in types" :key="`tipo${ index }`">
@@ -9,8 +36,8 @@
                 </li>
             </ul>
 
-        </div>
-    <div class="d-flex container flex-wrap resturant-container justify-content-center py-4">
+        </div> -->
+    <!-- <div class="d-flex container flex-wrap resturant-container justify-content-center py-4">
 
         <div v-for="resturant in resturants" :key="resturant.id" class="card" style="width: 18rem;">
             <img :src="resturant.image" class="card-img-top" alt="">
@@ -21,8 +48,9 @@
             </div>
          </div>
 
-     </div>
-    </div>
+      </div> -->
+  </div>
+    
  </template>
 
  <script>
@@ -82,7 +110,7 @@
      //     }
         },
      }
-     </script>
+  </script>
 
 <style lang="scss" scoped>
     @import'../../../sass/front/vars';
@@ -93,6 +121,89 @@
     .resturant-container{
         gap: 10px;
         border-radius: 10px;
-        background-color: $orange;
     }
+
+.restaurants{  
+  margin: 0 auto;
+  margin-top: 80px;
+}
+h2{
+  margin-bottom: 40px;
+  font-family: Poppins,sans-serif;
+}
+.input-wrap{
+  min-width: 300px;
+  height: 60px;
+  overflow: hidden;
+  padding: 0 2rem;
+}
+.inputs{
+  margin: 0 auto;
+  display:flex;
+  height:50px;
+  justify-content: center;
+  white-space: nowrap;
+  overflow-y: hidden;
+  overflow-x: scroll;
+  -webkit-overflow-scrolling: touch;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+}
+div label input {
+   margin-right:100px;
+}
+
+#ck-button {
+  background-color: $violet;
+  color: darken($violet, 50%);
+  margin:5px;
+  margin: 5px;
+  padding: 7px 20px;
+  border-radius:25px;
+  &:hover{
+    background-color: $violet;
+  }
+}
+
+#ck-button label {
+    float:left;
+    margin-bottom: 0;
+}
+
+#ck-button label span {
+    text-align:center;
+    display:block;
+}
+
+#ck-button label input {
+    position:absolute;
+    top:-20px;
+}
+
+#ck-button input:checked + span {
+    color:white;
+    padding: 0 30px 0 30px;
+    font-weight:600;
+    &:hover{
+      color: rgb(245, 68, 68);
+      background-color: $violet;
+    }
+}
+
+
+
+.line{
+  margin-top:50px;
+  width:93%;
+  margin: 0 auto;
+  height:3px;
+  background-color: black;
+}
+
+.card{
+  margin-top: 50px;
+  margin-bottom:30px;
+}
+
 </style>
